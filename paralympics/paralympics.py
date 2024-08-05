da0ab1ff1eec1238fce58488f9387a64629e49db
“Diar <diar.adekenov.20@ucl.ac.uk>
2024-08-05 20:29:34 +0100

    First Commit

---
+++ paralympics/paralympics.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from markupsafe import escape
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Hello and welcome to my paralympics app'
+
+@app.route('/hello/<username>')
+def hello(username):
+    return f'Hello {escape(username)} and welcome to my paralympics app'
+
+if __name__ == '__main__':
+    app.run(debug=True)
